68001e79ba55d8c78f67851568ff9c221675d596
xiaoming <917942168@qq.com>
2019-06-11 04:55:49 +0000

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,13 +4,13 @@
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
-<body>
+<body >
 @include('layouts._header')
-<div class="container">
+<div class="container" id="app">
     @include('shared._messages')
     @yield('content')
     @include('layouts._footer')
 </div>
-
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
