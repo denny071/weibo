32b4dcec7cdd83a7779ea0509f74e13e89524685
xiaoming <917942168@qq.com>
2019-06-12 04:47:32 +0000

    社交统计信息

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-         $this->call(UsersTableSeeder::class);
 
+        $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
 
         Model::reguard();
     }
