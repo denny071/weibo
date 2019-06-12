e2751a7ee48d56f96c72e2a0447df7e75aa11b36
xiaoming <917942168@qq.com>
2019-06-12 03:28:26 +0000

    粉丝数据表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -89,6 +89,25 @@ class UsersController extends Controller
         return back();
     }
 
+
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+
+
     protected function sendEmailConfirmationTo($user)
     {
         $view = 'emails.confirm';
