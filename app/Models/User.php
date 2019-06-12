96eb87354c0017210365ca26c5845b1ff97e77c6
xiaoming <917942168@qq.com>
2019-06-12 01:45:53 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,11 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
 
     public static function boot()
     {
